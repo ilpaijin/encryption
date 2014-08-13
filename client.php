2971bb09ddf3fffa28135c7d8707b1bef6d55b31
Ilpaijin <ilpaijin@gmail.com>
2014-08-13 11:24:38 +0200

    try/catch wrap

--- client.php
+++ client.php
@@ -2,27 +2,32 @@
 
 require_once "vendor/autoload.php";
 
-// $user = $this->getPostParam( 'user' );
-// $password = $this->getPostParam( 'password' );
-
 $password = "abz";
 
-$encrypter = Ilpaijin\Encrypter\EncrypterFactory::make('kakuna');
+try 
+{
+    $encrypter = Ilpaijin\Encrypter\EncrypterFactory::make('kakuna');
+    
+    $encryptedPassword = $encrypter->encrypt( $password );
+    var_dump($encryptedPassword);
 
-$encryptedPassword = $encrypter->encrypt( $password );
+    $decryptedPassword = $encrypter->decrypt( $encryptedPassword );
+    var_dump($decryptedPassword);
+} catch(Exception $e)
+{
+    echo $e->getMessage();
+}
 
-var_dump($encryptedPassword);
+try 
+{
+    $encrypter = Ilpaijin\Encrypter\EncrypterFactory::make('numeric');
 
-$decryptedPassword = $encrypter->decrypt( $encryptedPassword );
+    $encryptedPassword = $encrypter->encrypt( $password );
+    var_dump($encryptedPassword);
 
-var_dump($decryptedPassword);
-
-$encrypter = Ilpaijin\Encrypter\EncrypterFactory::make('numeric');
-
-$encryptedPassword = $encrypter->encrypt( $password );
-
-var_dump($encryptedPassword);
-
-$decryptedPassword = $encrypter->decrypt( $encryptedPassword );
-
-var_dump($decryptedPassword);
+    $decryptedPassword = $encrypter->decrypt( $encryptedPassword );
+    var_dump($decryptedPassword);
+} catch(Exception $e)
+{
+    echo $e->getMessage();
+}
